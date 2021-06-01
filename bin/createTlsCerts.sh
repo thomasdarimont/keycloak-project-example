39211568c2052cc7c262fbbe0cf8c9b7f27b99c1
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-01 09:40:31 +0200

    Fix mixup in setup tls script

--- bin/createTlsCerts.sh
+++ bin/createTlsCerts.sh
@@ -1,16 +1,14 @@
-@echo off
+#!/usr/bin/env bash
 
-if "%DOMAIN%"=="" (
- DOMAIN=acme.test
-)
+DOMAIN=${DOMAIN:-acme.test}
 
-echo "Generating TLS Certificate and Key for domain: %DOMAIN%"
+echo "Generating TLS Certificate and Key for domain: $DOMAIN"
 
 pushd .
-cd .\config\stage\dev\tls
-del /F *.pem
-mkcert -install %DOMAIN% "*.%DOMAIN%"
-echo "TLS Certificate and Key created in %CD%"
-dir /B *.pem
+cd ./config/stage/dev/tls
+rm -f *.pem
+mkcert -install $DOMAIN "*.$DOMAIN"
+echo "TLS Certificate and Key created in $PWD"
+ls -Al *.pem
 
 popd
