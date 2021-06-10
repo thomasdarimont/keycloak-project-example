2b75a34c3b38357d0a4d4d005e0e3a22f517ab99
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-10 20:56:01 +0200

    Add support for postgres database

--- start.java
+++ start.java
@@ -5,29 +5,54 @@ import java.util.Arrays;
 /**
  * Controller script to start the Keycloak environment.
  *
- * <h2>Running Keycloak with http</h2>
+ * <h2>Run Keycloak with http</h2>
  * <pre>{@code
  *  java start.java
  * }</pre>
  *
- * <h2>Running Keycloak with https</h2>
+ * <h2>Run Keycloak with https</h2>
  * <pre>{@code
  *  java start.java --https
  * }</pre>
  *
- * <h2>Running Keycloak with https and openldap</h2>
+ * <h2>Run Keycloak with https and openldap</h2>
  * <pre>{@code
  *  java start.java --https --openldap
  * }</pre>
+ *
+ * <h2>Run Keycloak with https, openldap and postgres database</h2>
+ * <pre>{@code
+ *  java start.java --https --openldap --postgres
+ * }</pre>
+ *
  */
 class start {
+
+    static final String HELP_CMD = "help";
+    static final String HTTPS_OPT = "--https";
+    static final String OPENLDAP_OPT = "--openldap";
+    static final String POSTGRES_OPT = "--postgres";
 
     public static void main(String[] args) {
 
         var argList = Arrays.asList(args);
 
-        boolean useHttps = argList.contains("--https") || argList.contains("--https=true");
-        boolean useOpenLdap = argList.contains("--openldap") || argList.contains("--openldap=true");
+        var useHttps = argList.contains(HTTPS_OPT) || argList.contains(HTTPS_OPT + "=true");
+        var useOpenLdap = argList.contains(OPENLDAP_OPT) || argList.contains(OPENLDAP_OPT + "=true");
+        var usePostgres = argList.contains(POSTGRES_OPT) || argList.contains(POSTGRES_OPT + "=true");
+
+        var showHelp = argList.contains(HELP_CMD);
+        if (showHelp) {
+            System.out.println("Keycloak Environment starter");
+            System.out.printf("%s supports the following options: %n", "start.java");
+            System.out.println("");
+            System.out.printf("%s: %s%n", HTTPS_OPT, "enables HTTPS support. (Optional) If not provided, plain HTTPS is used");
+            System.out.printf("%s: %s%n", OPENLDAP_OPT, "enables OpenLDAP support. (Optional)");
+            System.out.printf("%s: %s%n", POSTGRES_OPT, "enables postgrase database support. (Optional) If not provided, H2 database is used");
+            System.exit(0);
+        }
+
+        createFolderIfMissing("testrun/data");
 
         System.out.println("### Starting Keycloak Environment with HTTP" + (useHttps ? "S" : ""));
 
@@ -54,6 +79,13 @@ class start {
             dockerComposeCommandLine.add("docker-compose-openldap.yml");
         }
 
+        if (usePostgres) {
+            dockerComposeCommandLine.add("--file");
+            dockerComposeCommandLine.add("docker-compose-postgres.yml");
+
+            createFolderIfMissing("testrun/postgres/data/");
+        }
+
         dockerComposeCommandLine.add("up");
         dockerComposeCommandLine.add("--remove-orphans");
 
@@ -61,12 +93,20 @@ class start {
         pb.directory(new File("."));
         pb.inheritIO();
         try {
-            Process process = pb.start();
-            process.waitFor();
+            var process = pb.start();
+            System.exit(process.waitFor());
         } catch (Exception ex) {
             System.err.println("Could not start docker-compose.");
             ex.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    private static void createFolderIfMissing(String folderPath) {
+        var folder = new File(folderPath);
+        if (!folder.exists()) {
+            System.out.printf("Creating missing %s folder at %s success:%s%n"
+                    , folderPath, folder.getAbsolutePath(), folder.mkdirs());
         }
     }
 }
