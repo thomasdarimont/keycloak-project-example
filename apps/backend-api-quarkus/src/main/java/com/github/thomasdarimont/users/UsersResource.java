107c558f6fcf6142345473c9356e151458f06388
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-18 16:38:28 +0200

    Polishing

--- apps/backend-api-quarkus/src/main/java/com/github/thomasdarimont/users/UsersResource.java
+++ apps/backend-api-quarkus/src/main/java/com/github/thomasdarimont/users/UsersResource.java
@@ -3,15 +3,12 @@ package com.github.thomasdarimont.users;
 import io.quarkus.security.Authenticated;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.spi.HttpRequest;
 
-import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.List;
@@ -28,12 +25,12 @@ public class UsersResource {
     @Inject
     JsonWebToken jwt;
 
-    @Context
-    HttpRequest request;
-
     @GET
     @Path("/me")
     public Object me() {
+
+        log.infof("### Access me");
+
         Object username = jwt.getClaim("preferred_username");
         Map<String, Object> data = new HashMap<>();
         data.put("message", "Hello " + username);
@@ -48,7 +45,7 @@ public class UsersResource {
             @QueryParam("client_id") String clientId,
             @QueryParam("user_id") String userId
     ) {
-        log.infof("Generating dynamic claims for user. issuer=%s client_id=%s user_id=%s",
+        log.infof("### Generating dynamic claims for user. issuer=%s client_id=%s user_id=%s",
                 issuer, clientId, userId
         );
         Map<String, Object> data = new HashMap<>();
