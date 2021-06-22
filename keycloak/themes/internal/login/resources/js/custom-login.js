5517a5e86f24cf2e59b497573df056c039de2561
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-22 13:03:40 +0200

    Fix internationalization of SMS Authenticator

--- keycloak/themes/internal/login/resources/js/custom-login.js
+++ keycloak/themes/internal/login/resources/js/custom-login.js
@@ -2,5 +2,20 @@
 
 (function initTheme() {
     console.log("internal theme");
+
+    // hack to add mobile icon for sms authenticator, needs to be called after dom ready
+    function updateMobileIconOnSmsAuthenticatorInAuthenticationSelector() {
+        let elements = [...document.querySelectorAll('div.pf-c-title')].filter(elem => elem.textContent.includes('SMS'));
+        if (elements && elements.length > 0) {
+            console.log("patch mobile icon");
+            elements[0].parentElement.parentElement.querySelector("i").classList.add("fa-mobile");
+        }
+    }
+
+    function onDomContentLoaded() {
+        updateMobileIconOnSmsAuthenticatorInAuthenticationSelector();
+    }
+
+    document.addEventListener('DOMContentLoaded', evt => onDomContentLoaded());
 })();
 
