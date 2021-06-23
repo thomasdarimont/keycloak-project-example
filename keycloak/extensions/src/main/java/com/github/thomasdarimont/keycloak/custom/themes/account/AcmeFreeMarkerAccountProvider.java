dfce6d57e39f8b1fe61df51b05fda59073da7732
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-23 16:35:18 +0200

    Add support for demonstrating custom attributes in account page and update for phone_number via AIA required action

---
+++ keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/themes/account/AcmeFreeMarkerAccountProvider.java
@@ -0,0 +1,67 @@
+package com.github.thomasdarimont.keycloak.custom.themes.account;
+
+import freemarker.template.TemplateMethodModelEx;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.forms.account.AccountPages;
+import org.keycloak.forms.account.freemarker.FreeMarkerAccountProvider;
+import org.keycloak.models.Constants;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.services.Urls;
+import org.keycloak.theme.FreeMarkerUtil;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+
+public class AcmeFreeMarkerAccountProvider extends FreeMarkerAccountProvider {
+
+    public AcmeFreeMarkerAccountProvider(KeycloakSession session, FreeMarkerUtil freeMarker) {
+        super(session, freeMarker);
+    }
+
+    @Override
+    public Response createResponse(AccountPages page) {
+
+        if (attributes == null) {
+            attributes = new HashMap<>();
+        }
+
+        if (!attributes.containsKey("acmeAccountRequiredActionUrl")) {
+            // ensure required actions formatter is present
+            attributes.put("acmeAccountRequiredActionUrl", new AcmeRequiredActionUrlFormatterMethod(realm, uriInfo.getBaseUri()));
+        }
+
+        Response response = super.createResponse(page);
+        return response;
+    }
+
+    static class AcmeRequiredActionUrlFormatterMethod implements TemplateMethodModelEx {
+
+        private final String realm;
+        private final URI baseUri;
+
+        public AcmeRequiredActionUrlFormatterMethod(RealmModel realm, URI baseUri) {
+            this.realm = realm.getName();
+            this.baseUri = baseUri;
+        }
+
+        @Override
+        public Object exec(List list) {
+            String action = list.get(0).toString();
+
+            String authPath = "/auth/realms/" + realm + "/protocol/openid-connect/auth";
+            String redirectUri = Urls.accountBase(baseUri).build(realm).toString();
+
+            String url = UriBuilder.fromUri(baseUri).replacePath(authPath).queryParam(Constants.KC_ACTION, action)
+                    .queryParam(Constants.CLIENT_ID, "account")
+                    .queryParam(OAuth2Constants.REDIRECT_URI, redirectUri)
+                    .queryParam(OAuth2Constants.RESPONSE_TYPE, OAuth2Constants.CODE)
+                    .build().toString();
+
+            return url;
+        }
+    }
+}
---
+++ keycloak/extensions/src/main/resources/theme-resources/templates/update-phone-number-form.ftl
@@ -0,0 +1,34 @@
+<#import "template.ftl" as layout>
+<@layout.registrationLayout displayInfo=true; section>
+    <#if section = "title">
+        Update Mobile Phone Number
+    <#elseif section = "header">
+        Update Mobile Phone Number
+    <#elseif section = "form">
+
+        <p>Please update your mobile phone number</p>
+        <form id="kc-passwd-update-form" class="${properties.kcFormClass!}" action="${url.loginAction}" method="post">
+            <div class="${properties.kcFormGroupClass!}">
+                <div class="${properties.kcLabelWrapperClass!}">
+                    <label for="mobile">(Mobile) Phone Number</label>
+                </div>
+                <div class="${properties.kcInputWrapperClass!}">
+                    <input id="mobile" type="tel" name="mobile" value="${currentMobile}" required aria-invalid="<#if messagesPerField.existsError('mobile')>true</#if>"/>
+
+                    <#if messagesPerField.existsError('mobile')>
+                        <span id="input-error-mobile" class="${properties.kcInputErrorMessageClass!}" aria-live="polite">
+                            ${kcSanitize(messagesPerField.get('mobile'))?no_esc}
+                        </span>
+                    </#if>
+                </div>
+            </div>
+
+            <div class="${properties.kcFormGroupClass!}">
+                <div id="kc-form-buttons" class="${properties.kcFormButtonsClass!}">
+                    <input class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonLargeClass!}"
+                           type="submit" value="${msg("doSubmit")}"/>
+                </div>
+            </div>
+        </form>
+    </#if>
+</@layout.registrationLayout>
