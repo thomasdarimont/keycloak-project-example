e1aa8513b1cfa381b5dc5291a2abbad61593955e
Thomas Darimont <thomas.darimont@googlemail.com>
2021-07-16 01:21:37 +0200

    Add example bootstrap for adding custom attributes to login form template rendering

---
+++ keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/themes/login/AcmeFreeMarkerLoginFormsProvider.java
@@ -0,0 +1,27 @@
+package com.github.thomasdarimont.keycloak.custom.themes.login;
+
+import lombok.extern.jbosslog.JBossLog;
+import org.keycloak.forms.login.freemarker.FreeMarkerLoginFormsProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.theme.FreeMarkerUtil;
+import org.keycloak.theme.Theme;
+
+import javax.ws.rs.core.Response;
+import java.util.Locale;
+
+/**
+ * Custom {@link FreeMarkerLoginFormsProvider} to adjust the login form rendering context.
+ */
+@JBossLog
+public class AcmeFreeMarkerLoginFormsProvider extends FreeMarkerLoginFormsProvider {
+
+    public AcmeFreeMarkerLoginFormsProvider(KeycloakSession session, FreeMarkerUtil freeMarker) {
+        super(session, freeMarker);
+    }
+
+    @Override
+    protected Response processTemplate(Theme theme, String templateName, Locale locale) {
+        // expose custom objects in the template rendering via super.attributes
+        return super.processTemplate(theme, templateName, locale);
+    }
+}
