69fa7e45a77b4cbbc6a1f411351e2668183e61d7
Thomas Darimont <thomas.darimont@googlemail.com>
2021-09-10 17:28:09 +0200

    Refactor backend-api-spring

--- apps/backend-api-springboot/src/main/java/com/acme/backend/springboot/users/web/UsersController.java
+++ apps/backend-api-springboot/src/main/java/com/acme/backend/springboot/users/web/UsersController.java
@@ -1,6 +1,5 @@
 package com.acme.backend.springboot.users.web;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +10,11 @@ import org.springframework.web.context.request.ServletWebRequest;
 import java.util.HashMap;
 import java.util.Map;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
-@Slf4j
-@RequiredArgsConstructor
-public class UsersController {
+class UsersController {
 
     @GetMapping("/me")
     public Object me(ServletWebRequest request, Authentication authentication) {
@@ -23,8 +22,11 @@ public class UsersController {
         log.info("### Accessing {}", request.getRequest().getRequestURI());
 
         Object username = authentication.getName();
+
         Map<String, Object> data = new HashMap<>();
         data.put("message", "Hello " + username);
+        data.put("backend", "Spring Boot");
         return data;
     }
 }
+
