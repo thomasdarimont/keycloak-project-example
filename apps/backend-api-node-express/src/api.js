6785dc4749036fbc106cbaf88083da4f6cdcb41b
Thomas Darimont <thomas.darimont@googlemail.com>
2021-09-10 17:28:31 +0200

    Refactor backend-api-node-express

--- apps/backend-api-node-express/src/api.js
+++ apps/backend-api-node-express/src/api.js
@@ -5,7 +5,7 @@
  */
 function createApiEndpoints(app, config, LOG) {
 
-    LOG.info("Create API endpoints");
+    LOG.info('Create API endpoints');
 
     // API routes can then access JWT claims in the request object via request.user
     app.get('/api/users/me', (req, res) => {
@@ -16,6 +16,7 @@ function createApiEndpoints(app, config, LOG) {
 
         const data = {
             message: `Hello ${username}`,
+            backend: 'NodeJS Express',
         };
 
         res.status(200).send(JSON.stringify(data));
