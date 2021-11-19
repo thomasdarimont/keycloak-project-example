30e4ada042c8551f2a8a6b3f3349c16981fc64ff
Thomas Darimont <thomas.darimont@googlemail.com>
2021-11-19 23:22:44 +0100

    Refactor AccessFilter

--- keycloak/extensionsx/src/main/java/com/github/thomasdarimont/keycloakx/custom/security/AccessFilter.java
+++ keycloak/extensionsx/src/main/java/com/github/thomasdarimont/keycloakx/custom/security/AccessFilter.java
@@ -1,14 +1,13 @@
 package com.github.thomasdarimont.keycloakx.custom.security;
 
-import io.netty.handler.ipfilter.IpFilterRule;
 import io.netty.handler.ipfilter.IpFilterRuleType;
 import io.netty.handler.ipfilter.IpSubnetFilterRule;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.net.SocketAddress;
 import lombok.Data;
 import lombok.extern.jbosslog.JBossLog;
 import org.eclipse.microprofile.config.Config;
 import org.keycloak.configuration.Configuration;
+import org.keycloak.utils.StringUtil;
 
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -16,7 +15,6 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 import java.net.InetSocketAddress;
-import java.net.URI;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -27,6 +25,7 @@ public class AccessFilter implements ContainerRequestFilter {
 
     public static final String DEFAULT_IP_FILTER_RULES = "127.0.0.1/24,192.168.80.1/16,172.0.0.1/8";
     public static final String ADMIN_IP_FILTER_RULES_ALLOW = "acme.keycloak.admin.ip-filter-rules.allow";
+    public static final ForbiddenException FORBIDDEN_EXCEPTION = new ForbiddenException();
 
     private final PathIpFilterRules adminPathIpFilterRules;
 
@@ -34,35 +33,36 @@ public class AccessFilter implements ContainerRequestFilter {
     private HttpServerRequest httpServerRequest;
 
     public AccessFilter() {
-        Config config = Configuration.getConfig();
-        this.adminPathIpFilterRules = createAdminIpFilterRules(config);
-        log.infof("Created Security Filter rules for %s", adminPathIpFilterRules);
+        this.adminPathIpFilterRules = createAdminIpFilterRules(Configuration.getConfig());
     }
 
     private PathIpFilterRules createAdminIpFilterRules(Config config) {
 
-        String contextPath = config.getValue("quarkus.http.root-path", String.class);
-
-        String adminPath = makeContextPath(contextPath, "admin");
-
-        String filterRules = config //
+        var contextPath = config.getValue("quarkus.http.root-path", String.class);
+        var adminPath = makeContextPath(contextPath, "admin");
+        var filterRules = config //
                 .getOptionalValue(ADMIN_IP_FILTER_RULES_ALLOW, String.class) //
                 .orElse(DEFAULT_IP_FILTER_RULES);
+
+        if (StringUtil.isBlank(filterRules)) {
+            return null;
+        }
 
         var rules = new LinkedHashSet<IpSubnetFilterRule>();
         var ruleType = IpFilterRuleType.ACCEPT;
         var ruleDefinitions = List.of(filterRules.split(","));
 
-        for (String rule : ruleDefinitions) {
-            String[] tokens = rule.split("/");
-            String ip = tokens[0];
-            int cidrPrefix = Integer.parseInt(tokens[1]);
+        for (var rule : ruleDefinitions) {
+            var ipAndCidrPrefix = rule.split("/");
+            var ip = ipAndCidrPrefix[0];
+            var cidrPrefix = Integer.parseInt(ipAndCidrPrefix[1]);
             rules.add(new IpSubnetFilterRule(ip, cidrPrefix, ruleType));
         }
 
         var ruleDescription = adminPath + " " + ruleType + " from " + String.join(",", ruleDefinitions);
-
-        return new PathIpFilterRules(ruleDescription, adminPath, Set.copyOf(rules));
+        var pathIpFilterRules = new PathIpFilterRules(ruleDescription, adminPath, Set.copyOf(rules));
+        log.infof("Created Security Filter rules for %s", pathIpFilterRules);
+        return pathIpFilterRules;
     }
 
     private String makeContextPath(String contextPath, String subPath) {
@@ -75,25 +75,26 @@ public class AccessFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) {
 
-        URI requestUri = requestContext.getUriInfo().getRequestUri();
-        String requestPath = requestUri.getPath();
+        if (adminPathIpFilterRules == null) {
+            return;
+        }
+
+        var requestUri = requestContext.getUriInfo().getRequestUri();
         log.tracef("Processing request: %s", requestUri);
 
+        var requestPath = requestUri.getPath();
         if (requestPath.startsWith(adminPathIpFilterRules.getPathPrefix())) {
-            boolean requestAllowed = isAdminRequestAllowed();
-
-            if (!requestAllowed) {
-                throw new ForbiddenException();
+            if (!isAdminRequestAllowed()) {
+                throw FORBIDDEN_EXCEPTION;
             }
         }
     }
 
     private boolean isAdminRequestAllowed() {
 
-        SocketAddress remoteIp = httpServerRequest.connection().remoteAddress();
-
-        InetSocketAddress address = new InetSocketAddress(remoteIp.host(), remoteIp.port());
-        for (IpFilterRule filterRule : adminPathIpFilterRules.getIpFilterRules()) {
+        var remoteIp = httpServerRequest.connection().remoteAddress();
+        var address = new InetSocketAddress(remoteIp.host(), remoteIp.port());
+        for (var filterRule : adminPathIpFilterRules.getIpFilterRules()) {
             if (filterRule.matches(address)) {
                 return true;
             }
