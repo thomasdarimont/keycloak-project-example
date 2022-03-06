c49a644c6eccb113dc04af7473683aa37f4baa39
Thomas Darimont <thomas.darimont@googlemail.com>
2022-03-06 23:09:06 +0100

    backend-api-rust-actix polishing

--- apps/backend-api-rust-actix/src/middleware/jwt_auth.rs
+++ apps/backend-api-rust-actix/src/middleware/jwt_auth.rs
@@ -1,10 +1,10 @@
 use actix_4_jwt_auth::{OIDCValidator, OIDCValidatorConfig};
 use actix_web::rt::task;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use serde_json::Value;
 use std::collections::BTreeMap as Map;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct FoundClaims {
     pub iat: usize,
     pub exp: usize,
@@ -19,10 +19,7 @@ pub struct FoundClaims {
 
 impl FoundClaims {
     pub fn has_scope(&self, scope: &str) -> bool {
-        return self.scope
-            .split_ascii_whitespace()
-            .into_iter()
-            .any(|s| s == scope);
+        return self.scope.split_ascii_whitespace().into_iter().any(|s| s == scope);
     }
 }
 
@@ -33,8 +30,8 @@ pub async fn create_oidc_jwt_validator(issuer: &str) -> OIDCValidatorConfig {
         let validator = OIDCValidator::new_from_issuer(iss.clone()).unwrap();
         return OIDCValidatorConfig { issuer: iss, validator };
     })
-        .await
-        .unwrap();
+    .await
+    .unwrap();
 
     return config;
 }
