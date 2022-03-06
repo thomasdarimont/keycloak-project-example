1145872c38961952d5c219c5cccc08bb8869b283
Thomas Darimont <thomas.darimont@googlemail.com>
2022-03-06 11:30:35 +0100

    backend-api-rust-actix make config access explicit

--- apps/backend-api-rust-actix/src/main.rs
+++ apps/backend-api-rust-actix/src/main.rs
@@ -1,8 +1,7 @@
 #![feature(decl_macro)]
 
-use actix_web::{App, HttpServer};
 use actix_web::middleware::Logger;
-use config::Config;
+use actix_web::{App, HttpServer};
 
 mod api;
 mod config;
@@ -10,31 +9,24 @@ mod middleware;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let Config {
-        server_bind_addr,
-        cert_location,
-        key_location,
-        oidc_issuer,
-        allowed_cors_origin,
-        log_level_default,
-    } = config::Config::from_environment_with_defaults();
+    let config = config::Config::from_environment_with_defaults();
 
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or(&log_level_default));
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or(&config.log_level_default));
 
-    let ssl_acceptor_builder = middleware::ssl::create_ssl_acceptor(&cert_location, &key_location);
-    let oidc_jwt_validator = middleware::jwt_auth::create_oidc_jwt_validator(&oidc_issuer).await;
+    let ssl_acceptor_builder = middleware::ssl::create_ssl_acceptor(&config.cert_location, &config.key_location);
+    let oidc_jwt_validator = middleware::jwt_auth::create_oidc_jwt_validator(&config.oidc_issuer).await;
 
     HttpServer::new(move || {
-        let cors = middleware::cors::create_cors_config(allowed_cors_origin.clone());
+        let cors = middleware::cors::create_cors_config(config.allowed_cors_origin.clone());
 
         App::new()
-            .wrap(cors)
             // see https://actix.rs/actix-web/actix_web/middleware/struct.Logger.html
             .wrap(Logger::new("%a \"%r\" %s %b \"%{Referer}i\" %T"))
+            .wrap(cors)
             .app_data(oidc_jwt_validator.clone())
             .service(api::me_info::handle_me_info)
     })
-        .bind_openssl(server_bind_addr, ssl_acceptor_builder)?
-        .run()
-        .await
+    .bind_openssl(config.server_bind_addr, ssl_acceptor_builder)?
+    .run()
+    .await
 }
