5c1a0b715fd93cb5f53d9c932485c9c7a9599ac2
Thomas Darimont <thomas.darimont@googlemail.com>
2022-03-22 17:41:14 +0100

    keycloak: Adjust config extraction in AcmeUrlBean

--- keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/themes/login/AcmeUrlBean.java
+++ keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/themes/login/AcmeUrlBean.java
@@ -9,6 +9,7 @@ import org.keycloak.models.RealmModel;
 import java.util.Optional;
 
 public class AcmeUrlBean {
+
     private static final String ACME_SITE_URL_KEY = "acme_site_url";
 
     private static final String ACME_TERMS_URL_REALM_ATTRIBUTE_KEY = "acme_terms_url";
@@ -17,10 +18,10 @@ public class AcmeUrlBean {
     private static final String ACME_IMPRINT_URL_KEY = "acme_imprint_url";
 
     private static final String ACME_PRIVACY_URL_REALM_ATTRIBUTE_KEY = "acme_privacy_url";
-    public static final String ACME_PRIVACY_URL_CLIENT_ATTRIBUTE_KEY = "policyUri";
+    private static final String ACME_PRIVACY_URL_CLIENT_ATTRIBUTE_KEY = "policyUri";
 
     private static final String ACME_LOGO_URL_REALM_ATTRIBUTE_KEY = "acme_logo_url";
-    public static final String ACME_LOGO_URL_CLIENT_ATTRIBUTE_KEY = "logoUri";
+    private static final String ACME_LOGO_URL_CLIENT_ATTRIBUTE_KEY = "logoUri";
 
     private static final String ACME_ACCOUNT_DELETE_URL_KEY = "acme_account_deleted_url";
 
@@ -32,27 +33,31 @@ public class AcmeUrlBean {
     }
 
     public AcmeUrlBean(KeycloakSession session, ClientBean clientBean) {
-        RealmModel realm = session.getContext().getRealm();
+        var realm = session.getContext().getRealm();
         this.realmConfig = new RealmConfig(realm);
 
-        if(clientBean != null) {
+        if (clientBean != null) {
             this.clientConfig = new ClientConfig(realm.getClientByClientId(clientBean.getClientId()));
         } else {
             this.clientConfig = null;
         }
     }
 
-    /** BEGIN: Used from freemarker */
+    /**
+     * BEGIN: Used from freemarker
+     */
     public String getSiteUrl() {
         return realmConfig.getValue(ACME_SITE_URL_KEY);
     }
 
     public String getTermsUrl() {
-        return clientAttribute(ACME_TERMS_URL_CLIENT_ATTRIBUTE_KEY).orElse(realmConfig.getValue(ACME_TERMS_URL_REALM_ATTRIBUTE_KEY));
+        return clientAttribute(ACME_TERMS_URL_CLIENT_ATTRIBUTE_KEY) //
+                .orElse(realmConfig.getValue(ACME_TERMS_URL_REALM_ATTRIBUTE_KEY));
     }
 
     public String getPrivacyUrl() {
-        return clientAttribute(ACME_PRIVACY_URL_CLIENT_ATTRIBUTE_KEY).orElse(realmConfig.getValue(ACME_PRIVACY_URL_REALM_ATTRIBUTE_KEY));
+        return clientAttribute(ACME_PRIVACY_URL_CLIENT_ATTRIBUTE_KEY) //
+                .orElse(realmConfig.getValue(ACME_PRIVACY_URL_REALM_ATTRIBUTE_KEY));
     }
 
     public String getImprintUrl() {
@@ -61,7 +66,8 @@ public class AcmeUrlBean {
     }
 
     public String getLogoUrl() {
-        return clientAttribute(ACME_LOGO_URL_CLIENT_ATTRIBUTE_KEY).orElse(realmConfig.getValue(ACME_LOGO_URL_REALM_ATTRIBUTE_KEY));
+        return clientAttribute(ACME_LOGO_URL_CLIENT_ATTRIBUTE_KEY) //
+                .orElse(realmConfig.getValue(ACME_LOGO_URL_REALM_ATTRIBUTE_KEY));
     }
 
     public String getAccountDeletedUrl() {
@@ -69,14 +75,14 @@ public class AcmeUrlBean {
         return realmConfig.getValue(ACME_ACCOUNT_DELETE_URL_KEY);
     }
 
-    /** END: Used from freemarker */
+    /**
+     * END: Used from freemarker
+     */
 
     private Optional<String> clientAttribute(String key) {
-        if(this.clientConfig != null) {
+        if (this.clientConfig != null) {
             return Optional.ofNullable(this.clientConfig.getValue(key));
         }
         return Optional.empty();
     }
-
-
 }
