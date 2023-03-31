337b1b850cffee71e9a5dc7ad655f8323998fbaa
Thomas Darimont <thomas.darimont@googlemail.com>
2023-03-31 10:38:41 +0200

    Update to Keycloak 21.0.2

--- keycloak/extensions/src/test/java/com/github/thomasdarimont/keycloak/custom/KeycloakTestSupport.java
+++ keycloak/extensions/src/test/java/com/github/thomasdarimont/keycloak/custom/KeycloakTestSupport.java
@@ -40,7 +40,7 @@ public class KeycloakTestSupport {
     }
 
     public static KeycloakContainer createKeycloakContainer(String realmImportFileName) {
-        return createKeycloakContainer("quay.io/keycloak/keycloak:21.0.0", realmImportFileName);
+        return createKeycloakContainer("quay.io/keycloak/keycloak:21.0.2", realmImportFileName);
     }
 
     public static KeycloakContainer createKeycloakContainer(String imageName, String realmImportFileName) {
