db0ac4312c76040cfc7ee543633cceb5f53fd8bd
Thomas Darimont <thomas.darimont@googlemail.com>
2023-10-13 11:21:30 +0200

    Add help for --tracing to start.java

--- start.java
+++ start.java
@@ -311,6 +311,7 @@ class start {
         System.out.printf("  %s: %s%n", VERBOSE_OPT, "Shows debug information, such as the generated command");
         System.out.printf("  %s: %s%n", DOCKER_HOST_OPT, "Allows configuring of a non-default IP for reaching the docker host from inside the containers, " +
                 "which is used for name resolution. This is useful for using WiFi on ICE trains, which use the same network as docker by default. This causes the wifi to not work correctly.");
+        System.out.printf("  %s: %s%n", TRACING_OPT, "enables tracing with open-telemetry. Injects the otel agent into Keycloak, starts an otel-collector and jaeger container");
 
         System.out.printf("%n%s supports the following commands: %n", "start.java");
         System.out.println("");
