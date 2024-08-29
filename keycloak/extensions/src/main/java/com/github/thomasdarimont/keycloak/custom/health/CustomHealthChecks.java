14953df504c70d383c48072a23813c9bd627a743
Thomas Darimont <thomas.darimont@googlemail.com>
2024-08-29 10:38:00 +0200

    Revise custom health checks
    Database health checks are now provided by Keycloak itself.

--- keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/health/CustomHealthChecks.java
+++ keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/health/CustomHealthChecks.java
@@ -15,12 +15,10 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.lang.management.ManagementFactory;
 import java.sql.Connection;
+import java.time.Instant;
 
 /**
  * Example for custom health checks
- *
- * <p>Keycloak</p>
- * <a href="http://localhost:9990/health">Example Keycloak health checks</a>
  *
  * <p>Keycloak.X (with custom http-relative-path=/auth</p>
  * <a href="http://localhost:8080/auth/health">Example Keycloak.X health checks</a>
@@ -32,17 +30,10 @@ public class CustomHealthChecks {
     private static final HealthCheckResponseBuilder KEYCLOAK_SERVER_HEALTH_CHECK = //
             HealthCheckResponse.named("keycloak:server") //
                     .withData("version", Version.VERSION) //
-                    .withData("startupTimestamp", ManagementFactory.getRuntimeMXBean().getStartTime());
-
-    private static final int DB_CONN_VALID_TIMEOUT_MILLIS = 1000;
-
-    // JNDI Lookup ignored by Keycloak.X
-    @Resource(lookup = "java:jboss/datasources/KeycloakDS")
-    private DataSource keycloakDatasource;
+                    .withData("startupTime", Instant.ofEpochMilli(ManagementFactory.getRuntimeMXBean().getStartTime()).toString());
 
     /**
-     * <a href="http://localhost:9990/health/live">Example Keycloak liveness check</a>
-     * <a href="http://localhost:8080/auth/health/ready">Example Keycloak.X liveness check</a>
+     * <a href="http://localhost:8080/auth/health/live">Example Keycloak.X liveness check</a>
      *
      * @return
      */
@@ -54,41 +45,4 @@ public class CustomHealthChecks {
             return KEYCLOAK_SERVER_HEALTH_CHECK.up().build();
         };
     }
-
-    /**
-     * <a href="http://localhost:9990/health/ready">Example Keycloak readiness check</a>
-     * <a href="http://localhost:8080/auth/health/ready">Example Keycloak.X readiness check</a>
-     *
-     * @return
-     */
-    @Produces
-    @Readiness
-    HealthCheck databaseCheck() {
-        HealthCheckResponseBuilder databaseHealth = HealthCheckResponse.named("keycloak:database");
-
-        return () -> {
-            log.debug("Readiness check");
-            return (isDatabaseReady() ? databaseHealth.up() : databaseHealth.down()).build();
-        };
-    }
-
-    private boolean isDatabaseReady() {
-        DataSource dataSource = getDataSource();
-
-        if (dataSource == null) {
-            return false;
-        }
-
-        try (Connection con = dataSource.getConnection()) {
-            return con.isValid(DB_CONN_VALID_TIMEOUT_MILLIS);
-        } catch (Exception ex) {
-            return false;
-        }
-    }
-
-    private DataSource getDataSource() {
-        // Manual lookup for datasource via CDI
-        return CDI.current().select(DataSource.class).get();
-    }
-
 }
