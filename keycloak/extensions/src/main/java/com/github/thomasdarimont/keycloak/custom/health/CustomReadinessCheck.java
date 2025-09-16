caaae72d3362f36f86b6ec23cd54b106445fa055
Thomas Darimont <thomas.darimont@googlemail.com>
2025-09-16 15:33:01 +0200

    Add example for CustomReadinessCheck

---
+++ keycloak/extensions/src/main/java/com/github/thomasdarimont/keycloak/custom/health/CustomReadinessCheck.java
@@ -0,0 +1,37 @@
+package com.github.thomasdarimont.keycloak.custom.health;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.extern.jbosslog.JBossLog;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Readiness
+@JBossLog
+@ApplicationScoped
+public class CustomReadinessCheck implements HealthCheck {
+
+    private static final AtomicBoolean STOPPED = new AtomicBoolean(false);
+
+    @Override
+    public HealthCheckResponse call() {
+        return STOPPED.get() ? HealthCheckResponse.down("CONTAINER_STATUS") : HealthCheckResponse.up("CONTAINER_STATUS");
+    }
+
+    static {
+        log.info("Adding CustomReadinessCheck for SIG TERM");
+        SignalHandler signalHandler = sig -> {
+            log.infof("Detected SIG %s, marking this instance as unavailable", sig.getName());
+            STOPPED.set(true);
+        };
+        try {
+            Signal.handle(new Signal("TERM"), signalHandler);
+        } catch (Exception e) {
+            log.warnf("Failed to register signal handler: ", e.getMessage());
+        }
+    }
+}
