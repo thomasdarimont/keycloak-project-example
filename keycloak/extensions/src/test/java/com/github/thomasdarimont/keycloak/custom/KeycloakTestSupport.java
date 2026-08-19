3414b2d205a82d6330b7217202c43696982b3d25
Thomas Darimont <thomas.darimont@googlemail.com>
2026-08-19 10:09:45 +0200

    Update Keycloak version to 26.7.2

--- keycloak/extensions/src/test/java/com/github/thomasdarimont/keycloak/custom/KeycloakTestSupport.java
+++ keycloak/extensions/src/test/java/com/github/thomasdarimont/keycloak/custom/KeycloakTestSupport.java
@@ -42,7 +42,7 @@ public class KeycloakTestSupport {
     }
 
     public static KeycloakContainer createKeycloakContainer(String realmImportFileName) {
-        return createKeycloakContainer("quay.io/keycloak/keycloak:26.6.4", realmImportFileName);
+        return createKeycloakContainer("quay.io/keycloak/keycloak:26.7.2", realmImportFileName);
     }
 
     public static KeycloakContainer createKeycloakContainer(String imageName, String realmImportFileName) {
